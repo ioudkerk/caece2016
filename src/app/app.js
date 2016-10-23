4f91106496b7c0253ae803b5c5967d5987304a8a
shiva <ioudkerk@gmail.com>
2016-10-23 15:55:55 -0300

    auth y service ok

--- src/app/app.js
+++ src/app/app.js
@@ -18,16 +18,24 @@ angular.module('BlurAdmin', [
 ])
 
 .run(function($rootScope,$location,userIdentity){
+    console.log("cero");
     $rootScope.$on('$locationChangeStart', function(event) {
-      if ( ! userIdentity.logueado ){
-          var token = $location.search().token;
-          userIdentity.token(token);
-          if ( ! userIdentity.validar() ){
-              console.log("no estoy validado");
-              window.location = 'auth.html';
-          }
-          console.log("que onda?");
+        console.log("uno");
+        if ( ! userIdentity.logueado() ){
+          console.log("dos");
+          //var token = $location.search().token;
+          var token = "GfQtdIRieay5DKkw4gzoFvqjKnjnSNLi6Sl-ohCBjnV8TSpfOhalFpmuxIHsfQz9WyqAvBUovlmOcepFAQ3DNpocCTk4g1TTVBajuFMt82ZRvUA0nYAd8wrfSEEqgmrwRMGfUOBPm0BgYJPZLm_4IMFSTOROoLnPB0vO-I8C_esJZK3RAXH3exIP-c0KJgRk-tmi2DhcXCfip7hxkLgkp6Lph6lk_rfXWyaa4klQWltRkcyYb-5hbwgFgjSn4VY_GqZ8KH--Hr05ODtnktkvp0706zKjqAgq338x0gxj0TBVSAkPNmaxACPHjcDr4kLoMmUbKwW6IzRqYyhxhVP1i0Tp1DnHberAsTUCc1XC0lQTadhlQe186xKgjG-YT4X6WYBMk2wCRCNWQZMQTu-oUjC7HTZ357Eyp92HtaNYnkdvjNj6QmsHnRlfCTs-19ggZ_wPC2_X2dihLap6LlV1g6whM7eLCBsxSboo7PRDZqI";
+          userIdentity.setToken(token);
+          userIdentity.validar().then(
+              function(data){                  
+                  console.log(data)
+                },
+                function(){                    
+                    window.location = "auth.html";
+                }
+          );
+          
       }
+      console.log("tres");
     }
 )});
-
--- src/app/services.js
+++ src/app/services.js
@@ -1,19 +1,45 @@
 (function () {
   'use strict';
 
-  angular.module('BlurAdmin.services',[])
-      .factory('userIdentity', userIdentity);
+  var serverUrl="http://192.168.0.50:53395/";        
 
-  /** @ngInject */
-  function userIdentity() {
-    var token2;
-    return {
-      logueado: false,
-      token: function(token){ this.token = token },
-      validar: function(){
-          return this.token == "hola";
+ 
+
+  angular.module('BlurAdmin.services',[])      
+      .factory('userIdentity', userIdentity)
+      .factory('stockWebService', stockWebService);
+  
+  function userIdentity($http) {
+      var config = {  
+        status: {
+          logueado: false,
+        },
+        headers: {
+          Authorization: '',      
         }
+      };
+      
+      return {
+        logueado: function() {        
+          return config.status.logueado;
+        },
+        
+        setToken: function(t){ config.headers.Authorization = 'Bearer '+t },
+
+        validar : function(){
+          $http.defaults.headers.common['Authorization'] = config.headers.Authorization;
+          var url=serverUrl+"api/Account/UserInfo";          
+          return $http.get(url);
       }
+
     };
+
+  }
+
+  function stockWebService(){
+    return {
+        prueba: true,
+    }
+  };
 
 })();
