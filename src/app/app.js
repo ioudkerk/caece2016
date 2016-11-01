7d661867ea67a3ce4dd850aba88dab7a2d39eb1b
shiva <ioudkerk@gmail.com>
2016-11-01 01:19:17 -0300

    all done

--- src/app/app.js
+++ src/app/app.js
@@ -19,9 +19,9 @@ angular.module('BlurAdmin', [
 
 .run(function($rootScope,$location,userIdentity){
     //$rootScope.estaLogueado=false;
-    $rootScope.estaLogueado=true;
-    //var token = $location.search().token;
-    var token ="Hqy8bG_Yaw4yF4gx6GtXcrSFmsTgEOfV6OXRD5Wr2j27fjxY8vK76zxIxh4l4duvdjzdjrgPlPOWUfKUb16lmq28YP67QnvMwo-ZeA0_IpGjS2RTddrdn8C7tL0y1ncwCEiu7yvEygGDV9KtnvSsBDzRiDovcIqNw0aiVM0btj0_29wCeXO4RWeiP0XJ3JjkhX6t058uOAuk07YY2XQRO1s8xLVVk_c5NxShuwaGab-tsXJzyyI4xGbX_JC63QDmewh4H7aJdKpKMUX__dVgFpe32_GaauOYiO5ZypMi1ygN_cpjzwg99cRmlTZ9_v-16ZkwvAd-BT-1X4bXjjSMx2u1cLHLywOaIoz4Jyjd6LAhACmzApU20KMNXgT56FLf7-ZdE7MAv9VyX0cHVRUrPDydhS4DzMNjI5lVMFkBEKMevFvVry4qxxdMkithc8ftvpWyybBmKcEbpty60yQjT_hWg1Dh3zxo9t1Y9bkAuxjHeWI9tX6FuVsXak1JWUVh";          
+    $rootScope.estaLogueado=false;
+    var token = $location.search().token;
+    //var token ="Hqy8bG_Yaw4yF4gx6GtXcrSFmsTgEOfV6OXRD5Wr2j27fjxY8vK76zxIxh4l4duvdjzdjrgPlPOWUfKUb16lmq28YP67QnvMwo-ZeA0_IpGjS2RTddrdn8C7tL0y1ncwCEiu7yvEygGDV9KtnvSsBDzRiDovcIqNw0aiVM0btj0_29wCeXO4RWeiP0XJ3JjkhX6t058uOAuk07YY2XQRO1s8xLVVk_c5NxShuwaGab-tsXJzyyI4xGbX_JC63QDmewh4H7aJdKpKMUX__dVgFpe32_GaauOYiO5ZypMi1ygN_cpjzwg99cRmlTZ9_v-16ZkwvAd-BT-1X4bXjjSMx2u1cLHLywOaIoz4Jyjd6LAhACmzApU20KMNXgT56FLf7-ZdE7MAv9VyX0cHVRUrPDydhS4DzMNjI5lVMFkBEKMevFvVry4qxxdMkithc8ftvpWyybBmKcEbpty60yQjT_hWg1Dh3zxo9t1Y9bkAuxjHeWI9tX6FuVsXak1JWUVh";          
     userIdentity.setToken(token);
     userIdentity.validar().then(
         function(data){            
